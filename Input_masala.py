edb12eb3d2c304ec747d3851460d553ee4d7f481
A.R <roziboyev614@gmail.com>
2025-10-11 18:05:06 +0500

    masala yechish namuna

---
+++ Input_masala.py
@@ -0,0 +1,12 @@
+# # input operatoridan foydalanib Masala ishlash
+# a = int(input("Istalgan son kiriting men uni kub ildizini aniqlab beraman : "))
+# n =  a * a * a
+# print("Siz kiritgan sonning kub ildizi : ", n)
+
+# Kvadatning tomoni berilgan uning peremetrini aniqlash dasturini yozing
+
+# k_tomoni = int(input("Kvadratning tomonini kiriting : "))
+
+# p = k_tomoni * 4
+
+# print("Kvadratning peremetri : P = ", p)

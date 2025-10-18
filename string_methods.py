098e809997f77fefb10387d1a1843a08f94e38ac
A.R <roziboyev614@gmail.com>
2025-10-18 17:47:29 +0500

    string va f""

---
+++ string_methods.py
@@ -0,0 +1,26 @@
+# Bugun 18.10.2025 String Methods va f"" string formatlash haqida o'rganamiz
+
+# ism = 'Oyshajon'
+# status = 'Alo'
+# yoshi = 16
+
+# print(f"Hurmatli hurmatsiz oquvchi {ism} O'zlashtirishi {status} Yoshi {yoshi}")
+# print("Hurmatli hurmatsiz oquvchi" , ism ,"Ozlashtirishi ", status ,"Yoshi", yoshi)
+
+# ism1 = input("Guruh o'quvchilarining ismlarini kiriting  : ")
+# ism2 = input("2-chi o'quvchining ismini kiriting :")
+# ism3 = input("3-chi o'quvchining ismini kiriting :")
+# ism4 = input("4-chi o'quvchining ismini kiriting :")
+# ism5 = input("5-chi o'quvchining ismini kiriting :")
+
+# print(f"Guruh o'quvchilari : \n 1. {ism1} \n 2. {ism2.title()} \n 3. {ism3} \n 4. {ism4} \n 5. {ism5}")
+
+FIO = "murodov kamolbek sattarovich"
+print(FIO, "Asl korinishi")
+print(FIO.title(), "Title methodi bilan")
+print(FIO.upper(), "Upper methodi bilan")
+print(FIO.lower(), "Lower methodi bilan")
+print(FIO.capitalize(), "Capitalize methodi bilan")
+print(FIO.count('o'), "Count methodi bilan")
+print(FIO.find('sattarovich'), "Find methodi bilan")
+print(FIO.replace('murodov', 'abdullayev'), "Replace methodi bilan")

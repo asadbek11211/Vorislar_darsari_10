650ca0060bfeb6f22dbc1c67750aa27808386a35
A.R <roziboyev614@gmail.com>
2025-10-23 17:55:45 +0500

    listlar bilan amaliyot qildik

---
+++ Takrorlash_String_methods_list.py
@@ -0,0 +1,72 @@
+# ism = "oyshajon"
+# familiya = "ibragimova"
+# sharifi = "inoyatovna"
+# FIO = f"Hurmatli o'quvchi  {ism.title()} {familiya.upper()} {sharifi.title()}"
+# print(FIO)
+
+
+# ism = "Mohinur"
+# uy_manzili = "xorazm viloyati xonqa tumani qoraqosh qishlog'i gayrat mahallasi gayrat kochasi"
+# print(ism.lower())
+# print(ism.upper())
+# print(uy_manzili.capitalize())
+# print(uy_manzili.title())
+
+# mashina1 = "BMW"
+# mashina2 = "cobalt"
+# mashina3 = "gelik"
+# mashina4 = "jentra"
+
+
+
+# mashinalar = ["BMW", "cobalt", "gelik", "jentra"]
+# #              0        1        2         3
+# print(f"Bexruzbek to'yga {mashinalar[0]} da ketadi")
+# print(f"Bexruzbek to'yga {mashinalar[2].upper()} da ketadi")
+
+
+
+
+
+# mevalar = []
+# print(mevalar)
+
+# mevalar.append("kivi")
+# print(mevalar)
+
+# mevalar.append("anor")
+# print(mevalar)
+
+# mevalar.append("shaftoli")
+# print(mevalar)
+
+# mevalar.append("olma")
+# print(mevalar)
+
+# mevalar.append("banan")
+# print(mevalar)
+
+# mevalar.insert(2 ,"shaftoli qoqi")
+# print(mevalar)
+
+# mevalar.remove("shaftoli qoqi")
+# print(mevalar)
+
+
+# del mevalar[0]
+# print(mevalar)
+
+# mevalar.pop(0)
+# print(mevalar)
+
+# meva = mevalar.pop(0)
+# print(meva)
+
+
+# dostlar
+dostlar = ['bobur', 'karim', 'sulaymon']
+print(f"Salom {dostlar[0]} kecha {dostlar[1]} bilan suhbatlashdim. U aytdiki bugun biz men sen va {dostlar[2]} Kutubxonaga boramiz kelishdikmi")
+
+
+
+
---
+++ pythonda royhatlar.py
@@ -0,0 +1,53 @@
+"""
+21.10.2025
+Python da ro'yhatlar (lists) bilan ishlash
+
+"""
+# ism = 'Oyshajon'
+# status = 'Alo'
+# ism = 'Kumushoy'
+
+# Vorislar_10_sinf = ['oyshajon', 'kumushoy', 'mohinur', 'humoyun', 'behruzbek']
+
+#           #             0             1          2          3          4
+
+# ism1 = Vorislar_10_sinf[2]
+# print("3-chi o'quvchi ismi :", ism1.title())
+
+
+# mevalar = ['olma', 'banan', 'nok', 'shaftoli', 'uzum']
+# mashinalar = ['audi', 'bmw', 'mercedes', 'ferrari', 'lamborgini']
+# print(mevalar[3].upper())
+# print(mashinalar[1].title())
+# print("Men " + mashinalar[2].title() + "ni yaxshi ko'raman")
+
+
+hayvonlar = []
+print(hayvonlar)
+
+hayvonlar.append('it')
+print(hayvonlar)
+
+hayvonlar.append('mushuk')
+print(hayvonlar)
+
+hayvonlar.append('quyon')
+print(hayvonlar)
+
+hayvonlar.append('sichqon')
+print(hayvonlar)
+
+hayvonlar.append('tuzik_laqabli_kuchuk')
+print(hayvonlar)
+
+hayvonlar.insert(0, 'ot')
+print(hayvonlar)
+
+hayvonlar.insert(3, 'SIGIR')
+print(hayvonlar)
+
+hayvonlar.remove('SIGIR')
+print(hayvonlar)
+
+
+

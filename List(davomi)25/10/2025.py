6725b4cd7705b660e14d80bfb399e9d6a02f5249
A.R <roziboyev614@gmail.com>
2025-10-25 17:54:44 +0500

    Royhatlar bilan ishlash

---
+++ List(davomi)25/10/2025.py
@@ -0,0 +1,55 @@
+"""
+    10-sinf
+    25.10.2025
+    Ro'yhatlar bilan ishlash. Methodlar va function lar bilan ishlash
+    methods(.sort())
+    function(sum(), min(), max(), sorted(), list(),range())
+    Ro'yhatlardan nusxa olish
+    Tuple() o'zgarmas ro'yhat
+"""
+
+# Oyinlar = []
+# sportlar = list(())
+
+# print(Oyinlar)
+# print(sportlar)
+
+# Oyinlar.append("Plantes vc Zombie")
+# Oyinlar.append('GTA')
+# Oyinlar.append("DLS")
+# Oyinlar.append("PUBG")
+# Oyinlar.append("FIFA")
+
+# sportlar.insert(0, "boks")
+# sportlar.insert(1, "suzish")
+# sportlar.insert(2, "yugurish")
+# sportlar.insert(3, "sakrash")
+# sportlar.insert(4, "karate")
+# sportlar.insert(5, "judo")
+# sportlar.insert(6, "futbol")
+
+# print(Oyinlar)
+# print(sportlar)
+
+# Oyinlar.sort()
+# print(Oyinlar)
+
+# sportlar.sort()
+# print(sportlar)
+
+# sonlar = list(range(10,21,2))
+# print(sonlar)
+
+# toq_sonlar = list(range(1,100,2))
+# print(toq_sonlar)
+
+# juft_sonlar = list(range(2,101,2))
+# print(juft_sonlar)
+
+
+sonlar = [-98,58,63,1,1.2,1001,56,-120,-698,569,895,100]
+
+print(max(sonlar))
+print(min(sonlar))
+print(sum(sonlar))
+

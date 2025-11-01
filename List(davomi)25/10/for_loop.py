16d7b5f6d18aefde5a8d9025035425b8ac418f46
A.R <roziboyev614@gmail.com>
2025-11-01 17:51:57 +0500

    ssd

---
+++ List(davomi)25/10/for_loop.py
@@ -0,0 +1,39 @@
+"""
+    For loop sikl operatori
+"""
+
+# ismlar = ['oyshajon','kumushoy','mohinur','behruzbek','humoyun']
+
+# print(f"Assalomu alaykum {ismlar[0].title()} qalaysizlar. ")
+# print(f"Assalomu alaykum {ismlar[1].title()} qalaysizlar. ")
+# print(f"Assalomu alaykum {ismlar[2].title()} qalaysizlar. ")
+
+# print(f"Assalomu alaykum {ismlar[3].title()} qalaysizlar. ")
+# print(f"Assalomu alaykum {ismlar[4].title()} qalaysizlar. ")
+
+
+#sonlar  = list(range(1,41))
+# print(sonlar)
+# print(f"{sonlar[0]} ning kvadrati : {sonlar[0]**2}")
+# print(f"{sonlar[1]} ning kvadrati : {sonlar[1]**2}")
+
+
+
+# ismlar = ['oyshajon','kumushoy','mohinur','behruzbek','humoyun']
+# for mohinur in ismlar:
+#     print(f"Assalomu alaykum hurmatli : {mohinur.title()}")
+
+
+
+# sonlar = list(range(2,15,2))
+# print(f"Asl royhat : {sonlar}")
+
+# for son in sonlar:
+#     print(f"{son} ning kvadrati : {son**2}")
+# print("Tugadi")
+
+k = input("istalgan qiymat kiriting  qiymat kiriting : ")
+n = int(input("n ga qiymat kiriting (n>0): "))
+for k in n:
+    print(k)
+

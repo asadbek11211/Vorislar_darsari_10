61be9e184aec8b8be5121e45d711c6d02e2bb276
A.R <roziboyev614@gmail.com>
2025-12-06 17:21:33 +0500

    a

---
+++ Masala_ishlash.py
@@ -0,0 +1,21 @@
+# M = int(input("kg dagi qiymatni kiriting :"))
+# tonna = M/1000
+# print("Siz kiritgan qiymat ", M ,"kg ga teng")
+# print("tonnalarda esa ", tonna, "tonnaga teng")
+
+
+# A = int(input("Istalgan ikki honali son kiriting : " ))
+# birlar = A%10
+# onlar =int(A/10)
+# print("Siz kiritgan sonning onlar honasidagi raqam : ", onlar, "ga teng")
+# print("Siz kiritgan sonning birlar honasidagi raqam : ", birlar, "ga teng")
+
+
+B = int(input("Istalgan uch honali son kiriting "))
+birlar = B%10
+onlar =int(B/10)%10
+yuzlar = int(B/100)
+print(birlar)
+print(onlar)
+print(yuzlar)
+print("Siz kiritga sonning hona birliklari yigindisi : ", birlar + onlar + yuzlar, "ga teng")

884bcd53e3a47c1d78220ca6d0089fa94c3110cc
A.R <roziboyev614@gmail.com>
2025-12-09 16:11:31 +0500

    a

---
+++ dictiianary.py
@@ -0,0 +1,11 @@
+kamolbek = {
+    'ism' : 'kamolbek',
+    'yosh': 15,
+    'familya': 'rozmamatov',
+    'manzil': 'qora qosh',
+    'maktab': '1-sonli maktab',
+    'sinf': 9,
+    'qiziqish': 'fudbol',
+    'tranport': 'BMW',
+    'oqish': 'orta'
+}

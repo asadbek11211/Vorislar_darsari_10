83a777a39d5353689d03f2cbf0a073a588216a70
A.R <roziboyev614@gmail.com>
2025-12-09 15:45:03 +0500

    a5

--- Masala_ishlash.py
+++ Masala_ishlash.py
@@ -20,13 +20,27 @@
 # print(yuzlar)
 # print("Siz kiritga sonning hona birliklari yigindisi : ", birlar + onlar + yuzlar, "ga teng")
 
-l = int(input("baytlardagi qiymatni kiriting : "))
-m = l/1024
-print(l, "bayt ", m, "Kbayt ga teng")
+# l = int(input("baytlardagi qiymatni kiriting : "))
+# m = l/1024
+# print(l, "bayt ", m, "Kbayt ga teng")
 
+# son=int(input("Ixtiyoriy son kriting:"))
+# print(son**4)
 
+# son_1=int(input("1 ozgaruvchi"))
+# son_2=int(input("2 ozgaruvchi"))
+# son_3=int(input("3 ozgaruvchi"))
 
+# yigindi= (son_1+son_2-son_3)**3
+# print(yigindi)
 
+son1 =int(input("1-qiymatni kiriting/ .. /"))
+son2 =int(input("2-qiymatni kiriting/ .. /"))
+
+yigindi = (son1**3-son2**2)
+print(f"{son1} ning kubi {son1**3}")
+print(f"{son2} ning kvadrati{son2**2}")
+print(yigindi)
 
 
 

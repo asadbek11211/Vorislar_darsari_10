25900369e0706649fe690b9a482a0fa3b61169b2
A.R <roziboyev614@gmail.com>
2025-12-13 17:38:03 +0500

    rr

--- dictiianary.py
+++ dictiianary.py
@@ -1,22 +1,47 @@
 
 
-kamolbek = {
-    'ism' : 'kamolbek',
-    'yosh': 15,
-    'familya': 'rozmamatov',
-    'manzil': 'qora qosh',
-    'maktab': '1-sonli maktab',
-    'sinf': 9,
-    'qiziqish': 'fudbol',
-    'transport': 'BMW',
-    'oqish': 'orta'
+# kamolbek = {
+#     'ism' : 'kamolbek',
+#     'yosh': 15,
+#     'familya': 'rozmamatov',
+#     'manzil': 'qora qosh',
+#     'maktab': '1-sonli maktab',
+#     'sinf': 9,
+#     'qiziqish': 'fudbol',
+#     'transport': 'BMW',
+#     'oqish': 'orta'
+# }
+
+# print(f"{kamolbek['ism'].title()} {kamolbek['familya'].title()} {kamolbek['yosh']} yoshda ")
+
+
+
+cobalt = {
+    'ism' : 'cobalt',
+    'yosh': 10,
+    'TEZLIK': 180,
+    'RANG': 'oq',
+    'narxi': 40000,
+    'yili': 2015,
+    'karobka': 'avtomat',
+    'mashin tur': 'sedan',
+    'KM':70000,
+}
+print(f"ustozimning mashinasi {cobalt['ism'].upper()} {cobalt['KM']} km yurgan")
+
+malumotturlari = {
+    'string': 'asadbek',
+    'bulien': True,
+    'int': 5,
+    'float': 6.7,
+    'list':[3,6,8],
+    'typle':(3.14,9.8),
+    'dictionary':{
+        'meva': {
+            'sitrus':'ananas, kivi, apilsin, mandarin',
+            'quruq': 'anor, olma, shaftoli, orik'
+        },
+        'osimlik': 'tol'}
 }
 
-print(f"{kamolbek['ism'].title()} {kamolbek['familya'].title()} {kamolbek['yosh']} yoshda ")
-
-
-
-
-
-
-
+print(malumotturlari['dictionary']['meva']['sitrus'])

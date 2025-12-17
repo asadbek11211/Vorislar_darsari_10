497f32243c69a3a0855ebb690374f78a375c0f56
A.R <roziboyev614@gmail.com>
2025-12-17 15:51:17 +0500

    dgs

--- dictiianary.py
+++ dictiianary.py
@@ -16,32 +16,39 @@
 
 
 
-cobalt = {
-    'ism' : 'cobalt',
-    'yosh': 10,
-    'TEZLIK': 180,
-    'RANG': 'oq',
-    'narxi': 40000,
-    'yili': 2015,
-    'karobka': 'avtomat',
-    'mashin tur': 'sedan',
-    'KM':70000,
-}
-print(f"ustozimning mashinasi {cobalt['ism'].upper()} {cobalt['KM']} km yurgan")
+# cobalt = {
+#     'ism' : 'cobalt',
+#     'yosh': 10,
+#     'TEZLIK': 180,
+#     'RANG': 'oq',
+#     'narxi': 40000,
+#     'yili': 2015,
+#     'karobka': 'avtomat',
+#     'mashin tur': 'sedan',
+#     'KM':70000,
+# }
+# print(f"ustozimning mashinasi {cobalt['ism'].upper()} {cobalt['KM']} km yurgan")
 
-malumotturlari = {
-    'string': 'asadbek',
-    'bulien': True,
-    'int': 5,
-    'float': 6.7,
-    'list':[3,6,8],
-    'typle':(3.14,9.8),
-    'dictionary':{
-        'meva': {
-            'sitrus':'ananas, kivi, apilsin, mandarin',
-            'quruq': 'anor, olma, shaftoli, orik'
-        },
-        'osimlik': 'tol'}
-}
+# malumotturlari = {
+#     'string': 'asadbek',
+#     'bulien': True,
+#     'int': 5,
+#     'float': 6.7,
+#     'list':[3,6,8],
+#     'typle':(3.14,9.8),
+#     'dictionary':{
+#         'meva': {
+#             'sitrus':'ananas, kivi, apilsin, mandarin',
+#             'quruq': 'anor, olma, shaftoli, orik'
+#         },
+#         'osimlik': 'tol'}
+# }
 
-print(malumotturlari['dictionary']['meva']['sitrus'])
+# print(malumotturlari['dictionary']['meva']['sitrus'])
+
+
+
+print('5'+'2')
+print("Natija:", 5 + 2)
+print(10 / 3 * 3)
+type("Hello")
